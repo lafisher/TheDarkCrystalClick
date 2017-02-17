4280e93c64cff9cd9aefb7b904adfbdddefd18ed
Leigh Fisher <leighfisher@Jamess-MacBook-Air.local>
2017-02-17 00:08:03 -0600

    but it is not this day

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,16 @@
 numberToWin = Math.floor((Math.random() * 75) + 1);
 console.log(numberToWin)
+
+
+
+
+
+
+
+
+
+
+
+//"What was sundered and undone"
+//"Shall be whole the Two made One"
+//"You win!"
